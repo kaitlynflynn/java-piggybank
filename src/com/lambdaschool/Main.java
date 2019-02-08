bb221090b43c2122a3700a3cd6ab808ccda666f0
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2019-02-08 09:56:59 -0800

    Added ArrayList collection along with bank data; prints everything successfully to console

--- src/com/lambdaschool/Main.java
+++ src/com/lambdaschool/Main.java
@@ -1,10 +1,32 @@
 package com.lambdaschool;
 
+// Create a collection (array, arraylist, or hashmap) to represent the piggy bank. The collection can hold money.
+
+import java.util.ArrayList;
+
 public class Main
 {
 
     public static void main(String[] args)
     {
-        // write your code here
+        // Created collection using built in function of ArrayList
+        ArrayList<CashMoney> puggyBank = new ArrayList<>();
+        System.out.println("*** Kaitlyn's Puggy Bank ***");
+        puggyBank.add(new Quarter()); // adds Quarter
+        puggyBank.add(new Dime()); // adds Dime
+        puggyBank.add(new Dollar(5)); // adds 5 Dollars
+        puggyBank.add(new Nickle(3)); // adds 3 Nickles
+        puggyBank.add(new Dime(7)); // adds 7 Dimes
+        puggyBank.add(new Dollar()); // adds Dollar
+        puggyBank.add(new Penny(10)); // adds 10 Pennies
+
+        double total = 0;
+        for (CashMoney pug : puggyBank)
+        {
+            total += pug.getWorth();
+            System.out.println(pug.getDetails());
+        }
+        // Prints the value of the puggy bank
+        System.out.println("\n*** The puggy bank holds $" + total + " ***");
     }
 }
